532f0f2b34f0948ff9a7c679206a6ae45dfae295
Miguel Batista <miguel-borges20111@hotmail.com>
2021-11-02 12:09:36 -0300

    Basic static components

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,9 +1,16 @@
-function NavBar() {
-    return (
-      <div >
-        <h2>Dev superior</h2>
-      </div>
-    );
-  }
+import ImgDsDark from 'assets/img/ds-dark.svg';
 
-  export default NavBar;
+function NavBar() {
+  return (
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+      <div className="container">
+        <nav className="my-2 my-md-0 mr-md-3">
+          <img src={ImgDsDark} alt="DevSuperior" width="120" />
+        </nav>
+      </div>
+    </div>
+
+  );
+}
+
+export default NavBar;
